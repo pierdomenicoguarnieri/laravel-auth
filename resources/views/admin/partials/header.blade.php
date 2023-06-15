dd010cbbb95817bbcace24d08b474d3c8ef676b9
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-15 16:42:52 +0200

    add: views

---
+++ resources/views/admin/partials/header.blade.php
@@ -0,0 +1,51 @@
+<header>
+  <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+      <a class="navbar-brand d-flex align-items-center" href="{{ route('home') }}">
+        <div class="logo_laravel">
+          <img src="/img/logo.png" alt="Logo" width="30" class="d-inline-block align-text-top">
+        </div>
+        {{-- config('app.name', 'Laravel') --}}
+      </a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('home')}}">Vai al sito</a>
+          </li>
+        </ul>
+
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+          <!-- Authentication Links -->
+          @guest
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+
+            @if (Route::has('register'))
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+              </li>
+            @endif
+          @else
+            <li>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="ms-3">
+                @csrf
+
+                <button type="submit" class="btn btn-danger">Logout</button>
+              </form>
+            </div>
+            </li>
+          @endguest
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.guest')
+
+@section('title')
+  Home
+@endsection
+
+@section('content')
+  <main class="bg-dark text-white vh-100">
+    <div class="container py-5">
+      <h1 class="mb-4">Home</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eum nam voluptatibus dignissimos numquam. Iure eum mollitia, excepturi officia eos nihil nemo nostrum natus esse veniam, modi dicta dolorem perspiciatis.</p>
+    </div>
+  </main>
+@endsection
