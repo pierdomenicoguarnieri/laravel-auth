b8e9c2f247fe66683128bccd0c06999de82982e4
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-16 15:48:41 +0200

    updated: README.md

--- README.md
+++ README.md
@@ -2,5 +2,17 @@
 
 ## **Consegna:**
 
+### **Parte 1**
+
 Creiamo un nuovo progetto usando laravel breeze ed il pacchetto Laravel 9 Preset con autenticazione.
 Separamo gli ambienti Guest da quelli Admin per quanto riguarda stili, js, controller, viste e layout.
+
+### **Parte 2**
+
+Ognuno di voi immagini quali dati servono per un portfolio e quindi generate la migration.
+
+Il seeder è opzionale, l’importante che tutta la CRUD funzioni correttamente con tutte le funzionalità viste
+
+### **BONUS:**
+
+In edit e create sostituiamo la textarea con un CK Editor
--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -26,12 +26,12 @@
           <!-- Authentication Links -->
           @guest
             <li class="nav-item">
-              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+              <a class="nav-link" href="{{ route('login') }}">Login</a>
             </li>
 
             @if (Route::has('register'))
               <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                <a class="nav-link" href="{{ route('register') }}">Registrati</a>
               </li>
             @endif
           @else
